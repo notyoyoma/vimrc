79292975a67ea1aab32b408cca55469869d4c2a6
Marty Naselli <marty.naselli@gmail.com>
2014-11-05 16:26:05 -0500

    Alias wasn't working inside screens, so I used PATH instead

--- install-deps.sh
+++ install-deps.sh
@@ -3,7 +3,7 @@ mkdir ~/bin
 curl http://beyondgrep.com/ack-2.14-single-file > ~/bin/ack && chmod 0755 ~/bin/ack
 
 # Setup new software
-echo "alias ack='~/bin/ack'" > ~/.profile
-alias ack='~/bin/ack'
+echo "PATH=$PATH:~/bin/" > ~/.profile
+PATH=$PATH:~/bin/
 echo "runtime vimrc" > ~/.vimrc
 
