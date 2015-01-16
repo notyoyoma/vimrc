5b88d8c5335e1034bda0e1a8e0caf8e0fc42c924
Marty Naselli <marty.naselli@3foldx.com>
2015-01-16 15:17:06 +0000

    added wp-cli

--- install-deps.sh
+++ install-deps.sh
@@ -1,6 +1,7 @@
 # Install Ack
 mkdir -p ~/.local/bin
 curl http://beyondgrep.com/ack-2.14-single-file > ~/.local/bin/ack && chmod 0755 ~/.local/bin/ack
+curl https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar > ~/.local/bin/wp && chmod 0755 ~/.local/bin/wp
 
 # Setup new software
 echo "PATH=$PATH:~/.local/bin/" > ~/.profile
